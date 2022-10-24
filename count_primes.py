fedea5b8b3ab3994e4e06c3c08326027218134e2
Heidi E. Schmidt <bb00p04@gmail.com>
2022-10-24 14:24:23 -0400

    refactored

--- count_primes.py
+++ count_primes.py
@@ -1,27 +1,21 @@
-def count_primes(n):
+def count_prime_numbers(n):
     """
     This function takes in an array of numbers to count the prime numbers in it.
+    Reference: https://www.w3resource.com/python-exercises/basic/python-basic-1-exercise-68.php
 
     :param n:
     :return: int
     """
-    # array to contain the primes
-    prime_numbers = [2]
-    # array of primes to check against
-    # check each number against each of the prime_numbers array
-    for i in range (2,n):
-        is_prime = True
-        for prime in prime_numbers:
-            # if it is divisible by a prime number
-            #   then it is prime and it is added to prime_number list
-            if i % prime == 0:
-                print(i, "isn’t a prime number.")
-                is_prime = False
-                # Not sure how to exit
+    counter = 0
+    for num in range(n):
+        if num <= 1: # if num == 0 - skip it
+            continue
+        for idx in range(2, num): # for idx position in range 2 to number n
+            if (num % idx) == 0:  # this logic -- hmm
                 break
-        if is_prime:
-            prime_numbers.append(i)
-    return len(prime_numbers)
+        else:
+            counter += 1
+    return counter
 
-
-count_primes(20)
+print(count_prime_numbers(10))
+print(count_prime_numbers(100))
